87986a8284bc566136d7dffc684b7fb7c6b39c70
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2025-02-27 16:20:43 +0100

    création de l'interface

--- index.php
+++ index.php
@@ -3,8 +3,26 @@
 <head>
     <title>Messagerie instantanée</title>
     <style src="style.css"></style>
+    <link rel="icon" type="image/png" href="assets/messages-square.svg">
 </head>
 <body>
-    <h1>Messagerie instantanée</h1>
+    <div class="main">
+        <div class="header">
+            <h1>Messagerie instantanée</h1>
+        </div>
+        <div class="content">
+            <ul>
+                <li>
+                    msg 1
+                </li>
+                <li>
+                    msg 2
+                </li>
+            </ul>
+        </div>
+        <div class="footer">
+            <a href="">Rafraichir</a>
+        </div>
+    </div>
 </body>
 </html>
