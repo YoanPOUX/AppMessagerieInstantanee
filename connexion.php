5cb48e2f83c8055127b6d0c0b81977689554aabf
Mallart <matheo.allart09@gmail.com>
2025-03-18 10:29:17 +0100

    fixed messages saving

--- connexion.php
+++ connexion.php
@@ -1,11 +1,14 @@
 <?php
 // Informations de connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "projetr4a10";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connexion échouée: " . $conn->connect_error);
+
+function getPDO() : PDO
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "projetr4a10";
+
+    return new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 }
+
 ?>
