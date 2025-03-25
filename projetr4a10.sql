a0832e84ecf36d071e5e20e7fe8fed1da3617e8c
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2025-03-25 10:39:27 +0100

    wip

--- projetr4a10.sql
+++ projetr4a10.sql
@@ -17,6 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+CREATE USER IF NOT EXISTS 'iutadmin'@'localhost' IDENTIFIED BY '$iutinfo';
+
 REVOKE ALL PRIVILEGES ON `projetr4a10`.*
     FROM 'iutadmin'@'%';
 GRANT
